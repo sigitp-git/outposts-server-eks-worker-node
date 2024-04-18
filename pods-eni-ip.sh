03f6c78a67dc17144f1b51a93e320169e7ed172b
sigitp <sigit.priyanggoro@gmail.com>
2024-04-18 15:56:47 -0500

    Update pods-eni-ip.sh

--- pods-eni-ip.sh
+++ pods-eni-ip.sh
@@ -1,3 +1,21 @@
 https://github.com/aws/amazon-vpc-cni-k8s/blob/master/misc/eni-max-pods.txt
 
 https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-eni.html#AvailableIpPerENI
+
+
+aws ec2 describe-instance-types --filters "Name=instance-type,Values=c5.*" --query "InstanceTypes[].{Type: InstanceType, MaxENI: NetworkInfo.MaximumNetworkInterfaces, IPv4addr: NetworkInfo.Ipv4AddressesPerInterface}" --output table
+---------------------------------------
+|        DescribeInstanceTypes        |
++----------+----------+---------------+
+| IPv4addr | MaxENI   |     Type      |
++----------+----------+---------------+
+|  30      |  8       |  c5.4xlarge   |
+|  50      |  15      |  c5.24xlarge  |
+|  15      |  4       |  c5.xlarge    |
+|  30      |  8       |  c5.12xlarge  |
+|  10      |  3       |  c5.large     |
+|  15      |  4       |  c5.2xlarge   |
+|  50      |  15      |  c5.metal     |
+|  30      |  8       |  c5.9xlarge   |
+|  50      |  15      |  c5.18xlarge  |
++----------+----------+---------------+
