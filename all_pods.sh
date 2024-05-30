ae76e1424b0aee743e44f0845b5bdaf26b05073c
sigitp <sigit.priyanggoro@gmail.com>
2024-05-30 17:08:39 +0800

    Update all_pods.sh

--- all_pods.sh
+++ all_pods.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-
 # Store pod names in an array  
 # kubectl config set-context --current --namespace=kube-system
+
 pods=($(kubectl get po |
-    awk 'NR>1 { printf sep $2; sep=" "}'))
+    awk 'NR>1 { printf sep $1; sep=" "}'))
 
 if [[ ${#pods[@]} -gt $n ]]; then
     for pod in "${pods[@]}"; do
-        kubectl describe po "$pod" |
-        awk -v dt="$(date +"%a, %d %b %Y")" '
-            /SecretName:/ { next }
-            /Name:/ { name=$NF }
-            /Start Time:/ { t=$3 $4 $5 $6;
-                if (t==dt) print name
-                name="" }'
+        kubectl describe po "$pod"
     done
 fi
