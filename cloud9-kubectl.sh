dade949084f15cff5c602fea3e4ad1da8c12d3f3
sigitp <sigit.priyanggoro@gmail.com>
2024-07-25 23:04:45 -0500

    Create cloud9-kubectl.sh

---
+++ cloud9-kubectl.sh
@@ -0,0 +1,25 @@
+### KUBECTL FOR CLOUD9
+### ON CLOUD9, SETTINGS, AWS SETTINGS, TURN OFF AWS MANAGED TEMPORARY CREDENTIALS
+### ASIDE FROM DEFAULT CLOUD9SSM INSTANCE PROFILE POLICY, ADD EKS RBAC FOR CLOUD9 POLICY [policy-for-eks-rbac] 
+
+[cloudshell-user@ip-10-130-76-69 ~]$ cat aws-auth-cm-addrole.yaml 
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: arn:aws:iam::01234567890:role/AmazonEKSNodeRole
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+    - rolearn: arn:aws:iam::01234567890:role/role-eks-instance-profile-for-cloud9
+      username: i-0c9fe2ee9234098 ##EC2 instance ID of the Cloud9
+      groups:
+        - system:masters
+[cloudshell-user@ip-10-130-76-69 ~]$ 
+
+[cloudshell-user@ip-10-130-76-69 ~]$ kubectl apply -f aws-auth-cm-addrole.yaml 
+configmap/aws-auth configured
