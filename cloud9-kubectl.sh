6ecf14e60ba92e65284bc865546c75fec8775a7a
sigitp <sigit.priyanggoro@gmail.com>
2024-07-25 23:18:12 -0500

    Update cloud9-kubectl.sh

--- cloud9-kubectl.sh
+++ cloud9-kubectl.sh
@@ -1,6 +1,9 @@
 ### KUBECTL FOR CLOUD9
 ### ON CLOUD9, SETTINGS, AWS SETTINGS, TURN OFF AWS MANAGED TEMPORARY CREDENTIALS
 ### ASIDE FROM DEFAULT CLOUD9SSM INSTANCE PROFILE POLICY, ADD EKS RBAC FOR CLOUD9 POLICY [policy-for-eks-rbac.json] 
+### Attach a new role to the Cloud9 [kubectl-role-for-cloud9] instance with:
+### 1. AWSCloud9SSMInstanceProfile | AWS managed
+### 2. policy-for-eks-rbac | Customer managed
 
 [cloudshell-user@ip-10-130-76-69 ~]$ cat aws-auth-cm-addrole.yaml 
 apiVersion: v1
