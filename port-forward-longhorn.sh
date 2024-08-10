a196826df278db31e0c61914a2a9056f232a2016
sigitp <sigit.priyanggoro@gmail.com>
2024-08-09 21:00:10 -0500

    Update port-forward-longhorn.sh

--- port-forward-longhorn.sh
+++ port-forward-longhorn.sh
@@ -1,3 +1,8 @@
 # port-forward-longhorn-via-worker-node
+
+kubectl get svc -A
+kubectl describe svc longhorn-frontend -n longhorn-system
+
+# port forward the endpoint IP and port via SSH to worker node
 ssh -i file.pem -L 8000:172.31.149.220:8000 ec2-user@ec2-184-72-87-109.compute-1.amazonaws.com
 ssh -i file.pem -L 8000:172.31.151.158:8000 ec2-user@ec2-34-229-218-164.compute-1.amazonaws.com
