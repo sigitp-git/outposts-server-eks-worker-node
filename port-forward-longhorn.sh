6a17a039cbf7dce61d358561d68041e9b4c94681
sigitp <sigit.priyanggoro@gmail.com>
2024-08-09 20:56:59 -0500

    Update port-forward-longhorn.sh

--- port-forward-longhorn.sh
+++ port-forward-longhorn.sh
@@ -1,1 +1,3 @@
+# port-forward-longhorn-via-worker-node
+ssh -i file.pem -L 8000:172.31.149.220:8000 ec2-user@ec2-184-72-87-109.compute-1.amazonaws.com
 ssh -i file.pem -L 8000:172.31.151.158:8000 ec2-user@ec2-34-229-218-164.compute-1.amazonaws.com
