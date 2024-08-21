b237321528c863ccbd17defe9812622424262d27
sigitp <sigit.priyanggoro@gmail.com>
2024-08-21 15:50:40 -0500

    Update scp-kernel-cloud9-upload-local-file.sh

--- scp-kernel-cloud9-upload-local-file.sh
+++ scp-kernel-cloud9-upload-local-file.sh
@@ -17,6 +17,7 @@ scp -i "5gc-pmt-keypair.pem" ./kernel-6.1.103-111.183.amzn2023.x86_64.rpm ec2-us
 scp -i "5gc-pmt-keypair.pem" ./kernel-6.1.103-111.183.amzn2023.x86_64.rpm ec2-user@ec2-34-229-218-164.compute-1.amazonaws.com:/home/ec2-user
 
 # SSM SCP
+# https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-getting-started-enable-ssh-connections.html
 scp -i "5gc-pmt-keypair.pem" ./kernel-6.1.103-111.183.amzn2023.x86_64.rpm ec2-user@i-06a97c36948380254:/home/ec2-user
 scp -i "5gc-pmt-keypair.pem" ./kernel-6.1.103-111.183.amzn2023.x86_64.rpm ec2-user@i-0daa0707552aa7a8b:/home/ec2-user
 scp -i "5gc-pmt-keypair.pem" ./kernel-6.1.103-111.183.amzn2023.x86_64.rpm ec2-user@i-0aaac0481504ad6a7:/home/ec2-user
