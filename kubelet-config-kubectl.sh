a6e36a0a9ef4e0c6e1dd8996d2dcd35d1562cc4b
sigitp <sigit.priyanggoro@gmail.com>
2024-08-26 20:37:07 -0500

    Update kubelet-config-kubectl.sh

--- kubelet-config-kubectl.sh
+++ kubelet-config-kubectl.sh
@@ -1,4 +1,10 @@
-Admin:~/environment $ kubectl get --raw "/api/v1/nodes/ip-172-31-149-12.ec2.internal/proxy/configz" | jq                                                                                          
+Admin:~/environment $ kubectl get nodes
+NAME                          STATUS   ROLES    AGE     VERSION
+ip-10-0-57-154.ec2.internal   Ready    <none>   3h32m   v1.30.2-eks-1552ad0
+ip-10-0-62-122.ec2.internal   Ready    <none>   3h27m   v1.30.2-eks-1552ad0
+
+
+Admin:~/environment $ kubectl get --raw "/api/v1/nodes/ip-10-0-57-154.ec2.internal/proxy/configz" | jq                                              
 {
   "kubeletconfig": {
     "enableServer": true,
@@ -48,7 +54,7 @@ Admin:~/environment $ kubectl get --raw "/api/v1/nodes/ip-172-31-149-12.ec2.inte
     "oomScoreAdj": -999,
     "clusterDomain": "cluster.local",
     "clusterDNS": [
-      "10.100.0.10"
+      "172.20.0.10"
     ],
     "streamingConnectionIdleTimeout": "4h0m0s",
     "nodeStatusUpdateFrequency": "10s",
@@ -69,7 +75,7 @@ Admin:~/environment $ kubectl get --raw "/api/v1/nodes/ip-172-31-149-12.ec2.inte
     "topologyManagerScope": "container",
     "runtimeRequestTimeout": "2m0s",
     "hairpinMode": "hairpin-veth",
-    "maxPods": 234,
+    "maxPods": 737,
     "podPidsLimit": -1,
     "resolvConf": "/etc/resolv.conf",
     "cpuCFSQuota": true,
@@ -102,9 +108,9 @@ Admin:~/environment $ kubectl get --raw "/api/v1/nodes/ip-172-31-149-12.ec2.inte
     "containerLogMonitorInterval": "10s",
     "configMapAndSecretChangeDetectionStrategy": "Watch",
     "kubeReserved": {
-      "cpu": "150m",
+      "cpu": "550m",
       "ephemeral-storage": "1Gi",
-      "memory": "2829Mi"
+      "memory": "8362Mi"
     },
     "systemReservedCgroup": "/system",
     "kubeReservedCgroup": "/runtime",
@@ -112,7 +118,7 @@ Admin:~/environment $ kubectl get --raw "/api/v1/nodes/ip-172-31-149-12.ec2.inte
       "pods"
     ],
     "volumePluginDir": "/usr/libexec/kubernetes/kubelet-plugins/volume/exec/",
-    "providerID": "aws:///us-east-1d/i-0160c5cd534028fbb",
+    "providerID": "aws:///us-east-1a/i-0d3ba9c35f51be349",
     "logging": {
       "format": "text",
       "flushFrequency": "5s",
@@ -139,4 +145,148 @@ Admin:~/environment $ kubectl get --raw "/api/v1/nodes/ip-172-31-149-12.ec2.inte
     "containerRuntimeEndpoint": "unix:///run/containerd/containerd.sock"
   }
 }
-Admin:~/environment $
+Admin:~/environment $ 
+
+
+Admin:~/environment $ kubectl get --raw "/api/v1/nodes/ip-10-0-62-122.ec2.internal/proxy/configz" | jq                                             
+{
+  "kubeletconfig": {
+    "enableServer": true,
+    "podLogsDir": "/var/log/pods",
+    "syncFrequency": "1m0s",
+    "fileCheckFrequency": "20s",
+    "httpCheckFrequency": "20s",
+    "address": "0.0.0.0",
+    "port": 10250,
+    "tlsCipherSuites": [
+      "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+      "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+      "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305",
+      "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+      "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+      "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",
+      "TLS_RSA_WITH_AES_128_GCM_SHA256",
+      "TLS_RSA_WITH_AES_256_GCM_SHA384"
+    ],
+    "serverTLSBootstrap": true,
+    "authentication": {
+      "x509": {
+        "clientCAFile": "/etc/kubernetes/pki/ca.crt"
+      },
+      "webhook": {
+        "enabled": true,
+        "cacheTTL": "2m0s"
+      },
+      "anonymous": {
+        "enabled": false
+      }
+    },
+    "authorization": {
+      "mode": "Webhook",
+      "webhook": {
+        "cacheAuthorizedTTL": "5m0s",
+        "cacheUnauthorizedTTL": "30s"
+      }
+    },
+    "registryPullQPS": 5,
+    "registryBurst": 10,
+    "eventRecordQPS": 50,
+    "eventBurst": 100,
+    "enableDebuggingHandlers": true,
+    "healthzPort": 10248,
+    "healthzBindAddress": "127.0.0.1",
+    "oomScoreAdj": -999,
+    "clusterDomain": "cluster.local",
+    "clusterDNS": [
+      "172.20.0.10"
+    ],
+    "streamingConnectionIdleTimeout": "4h0m0s",
+    "nodeStatusUpdateFrequency": "10s",
+    "nodeStatusReportFrequency": "5m0s",
+    "nodeLeaseDurationSeconds": 40,
+    "imageMinimumGCAge": "2m0s",
+    "imageMaximumGCAge": "0s",
+    "imageGCHighThresholdPercent": 85,
+    "imageGCLowThresholdPercent": 80,
+    "volumeStatsAggPeriod": "1m0s",
+    "cgroupRoot": "/",
+    "cgroupsPerQOS": true,
+    "cgroupDriver": "systemd",
+    "cpuManagerPolicy": "static",
+    "cpuManagerReconcilePeriod": "10s",
+    "memoryManagerPolicy": "None",
+    "topologyManagerPolicy": "single-numa-node",
+    "topologyManagerScope": "container",
+    "runtimeRequestTimeout": "2m0s",
+    "hairpinMode": "hairpin-veth",
+    "maxPods": 737,
+    "podPidsLimit": -1,
+    "resolvConf": "/etc/resolv.conf",
+    "cpuCFSQuota": true,
+    "cpuCFSQuotaPeriod": "100ms",
+    "nodeStatusMaxImages": 50,
+    "maxOpenFiles": 1000000,
+    "contentType": "application/vnd.kubernetes.protobuf",
+    "kubeAPIQPS": 50,
+    "kubeAPIBurst": 100,
+    "serializeImagePulls": false,
+    "evictionHard": {
+      "memory.available": "100Mi",
+      "nodefs.available": "10%",
+      "nodefs.inodesFree": "5%"
+    },
+    "evictionPressureTransitionPeriod": "5m0s",
+    "enableControllerAttachDetach": true,
+    "protectKernelDefaults": true,
+    "makeIPTablesUtilChains": true,
+    "iptablesMasqueradeBit": 14,
+    "iptablesDropBit": 15,
+    "featureGates": {
+      "RotateKubeletServerCertificate": true
+    },
+    "failSwapOn": true,
+    "memorySwap": {},
+    "containerLogMaxSize": "10Mi",
+    "containerLogMaxFiles": 5,
+    "containerLogMaxWorkers": 1,
+    "containerLogMonitorInterval": "10s",
+    "configMapAndSecretChangeDetectionStrategy": "Watch",
+    "kubeReserved": {
+      "cpu": "550m",
+      "ephemeral-storage": "1Gi",
+      "memory": "8362Mi"
+    },
+    "systemReservedCgroup": "/system",
+    "kubeReservedCgroup": "/runtime",
+    "enforceNodeAllocatable": [
+      "pods"
+    ],
+    "volumePluginDir": "/usr/libexec/kubernetes/kubelet-plugins/volume/exec/",
+    "providerID": "aws:///us-east-1a/i-077f4082abbffb441",
+    "logging": {
+      "format": "text",
+      "flushFrequency": "5s",
+      "verbosity": 2,
+      "options": {
+        "text": {
+          "infoBufferSize": "0"
+        },
+        "json": {
+          "infoBufferSize": "0"
+        }
+      }
+    },
+    "enableSystemLogHandler": true,
+    "enableSystemLogQuery": false,
+    "shutdownGracePeriod": "0s",
+    "shutdownGracePeriodCriticalPods": "0s",
+    "enableProfilingHandler": true,
+    "enableDebugFlagsHandler": true,
+    "seccompDefault": false,
+    "memoryThrottlingFactor": 0.9,
+    "registerNode": true,
+    "localStorageCapacityIsolation": true,
+    "containerRuntimeEndpoint": "unix:///run/containerd/containerd.sock"
+  }
+}
+Admin:~/environment $ 
