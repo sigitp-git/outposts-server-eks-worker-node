3e70c6f7b47d28dbf152bc9f0b2aa599f8d892f4
sigitp <sigit.priyanggoro@gmail.com>
2025-02-12 15:40:22 -0600

    Update longhorn-pv-pvc-cleanup.sh

--- longhorn-pv-pvc-cleanup.sh
+++ longhorn-pv-pvc-cleanup.sh
@@ -4,6 +4,7 @@ kubectl patch pv <pv_name> -p '{"metadata": {"finalizers": null}}'
 kubectl delete pv <pv_name> -n namespace --grace-period=0 --force
 
 ## PVC cleanup
+kubectl delete pvc <pvc_name> -n namespace --grace-period=0 --force
 kubectl edit pvc {your-pvc-name} -n namespace
 
 ## Remove finalizer from pvc configuration.
