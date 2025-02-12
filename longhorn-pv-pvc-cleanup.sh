68651e1852908980f39960a158f2d71ef52d90c7
sigitp <sigit.priyanggoro@gmail.com>
2025-02-12 15:39:18 -0600

    Update longhorn-pv-pvc-cleanup.sh

--- longhorn-pv-pvc-cleanup.sh
+++ longhorn-pv-pvc-cleanup.sh
@@ -1,4 +1,11 @@
 ## PV cleanup
+## https://stackoverflow.com/questions/54629660/kubernetes-how-do-i-delete-pv-in-the-correct-manner
 kubectl patch pv <pv_name> -p '{"metadata": {"finalizers": null}}'
 kubectl delete pv <pv_name> -n namespace --grace-period=0 --force
 
+## PVC cleanup
+kubectl edit pvc {your-pvc-name} -n namespace
+
+## Remove finalizer from pvc configuration.
+finalizers:
+  -  kubernetes.io/pv-protection
